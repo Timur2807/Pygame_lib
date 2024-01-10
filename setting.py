dadcc1b5805cc8c651f934bf905e4ceb5951c412
Тимур <langress3@yandex.ru>
2024-01-10 19:44:13 +0300

    немного написал пришелцев

---
+++ setting.py
@@ -0,0 +1,6 @@
+class Settings():
+    """Класс прнимает настройки мини экрана."""
+    def __init__(self):
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = 0,0,0
